a1ea842fbdbad5a6dd9b2a95b0a7816b0e3deb19
Nicolas Faye <85998620+Nixa001@users.noreply.github.com>
2023-02-20 23:20:53 +0000

    Feat: syntax

--- hackaton_1.go
+++ hackaton_1.go
@@ -9,15 +9,13 @@ import (
 
 func main() {
 	var (
-		designation string
-		quantite    int
+		designation                    string
+		quantite                       int
 		prixUnit, total, payee, change float64
 	)
 
-	// Créer un lecteur de saisie utilisateur
 	reader := bufio.NewReader(os.Stdin)
 
-	// Saisir et traiter les données pour chaque article
 	for {
 		fmt.Println("Entrez la désignation de l'article (ou tapez 'q' pour quitter) :")
 		designation, _ = reader.ReadString('\n')
@@ -33,19 +31,14 @@ func main() {
 		fmt.Println("Entrez le prix unitaire :")
 		fmt.Scan(&prixUnit)
 
-		// Mettre à jour le total
 		total += float64(quantite) * prixUnit
 	}
 
-	// Afficher le montant total
 	fmt.Printf("Le montant total est de %.2f\n", total)
 
-	// Saisir et traiter la somme versée
 	fmt.Println("Entrez la somme versée :")
 	fmt.Scan(&payee)
 
-	// Calculer et afficher la monnaie
 	change = payee - total
 	fmt.Printf("Le montant de la monnaie est de %.2f\n", change)
 }
-
